e3d45f82e6d75cba8c52a5ced94e6222d1617375
nico <513993979@qq.com>
2024-10-06 21:53:24 +0800

    Update TheWelcome.vue

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,11 +12,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    <a href="https://wood.uinio.pro/" target="_blank" rel="noopener">官网①: wood.uinio.pro</a>
   </WelcomeItem>
 
   <WelcomeItem>
